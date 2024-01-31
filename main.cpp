6b015c6a83c17e92727afe00bc01198d3e01a19b
shadmanhossain <shadmanh@sfu.ca>
2024-01-30 23:36:42 -0800

    Commented out tests for bank

--- main.cpp
+++ main.cpp
@@ -6,28 +6,28 @@
 
 //std::cout;
 //std::endl;
-void insertion_test(){
-  Bank bank;
-  bank.insert(12345, "John Doe", 2000, "Chequings");
-  cout << "The Bank has # " << bank.size() << " accounts" <<endl;
-  bank.insert(23456, "Jane Doe", 10000, "Savings");
-  cout << "The Bank has # " << bank.size() << " accounts" <<endl;
-  // bank.print(bank.root);
-}
+// void insertion_test(){
+//   Bank bank;
+//   bank.createAccount(12345, "John Doe", "Chequings");
+//   cout << "The Bank has # " << bank.totalNumberOfAccounts() << " accounts" <<endl;
+//   bank.createAccount(23456, "Jane Doe", "Savings");
+//   cout << "The Bank has # " << bank.size() << " accounts" <<endl;
+//   // bank.print(bank.root);
+// }
 
-void remove_test(){
-  Bank bank;
-  bank.insert(12345, "John Doe", 2000, "Chequings");
-  bank.insert(23456, "Jane Doe", 10000, "Savings");
-  //bank.print(bank.root);
-  cout << "The Bank has # " << bank.size() << " accounts" <<endl;
-  // bank.remove(12345);
-  bank.remove(23456);
-  cout << "The Bank has # " << bank.size() << " accounts" <<endl;
-}
+// void remove_test(){
+//   Bank bank;
+//   bank.insert(12345, "John Doe", 2000, "Chequings");
+//   bank.insert(23456, "Jane Doe", 10000, "Savings");
+//   //bank.print(bank.root);
+//   cout << "The Bank has # " << bank.size() << " accounts" <<endl;
+//   // bank.remove(12345);
+//   bank.remove(23456);
+//   cout << "The Bank has # " << bank.size() << " accounts" <<endl;
+// }
 
 int main() {
-  insertion_test();
+  // insertion_test();
   // remove_test();
   return 0;
 }
