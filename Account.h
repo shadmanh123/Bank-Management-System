53d74632b85852f254bc10fd6be47ca55ba83dd5
shadmanhossain <shadmanh@sfu.ca>
2024-01-30 23:33:30 -0800

    Fixing Account.h, Bank.h, and Bank.cpp

--- Account.h
+++ Account.h
@@ -3,14 +3,15 @@
 #include <string>
 using std::string;
 class Account{
-  public:
-    int account_num;//account number
+  private:
+    unsigned int account_number;//account number only positive to provide greater range of numbers
     string name; //account holder name
-    int balance; //account balance
-    string account_type; //account type
-    Account(int key_data, string v_data){ //constructor
-      account_num = key_data;
-      name = v_data;
-      balance = 0;
-    }
+    int balance; //account balance can be negative
+    string account_type; //account type as in Chequeings vs Savings
+  public:
+    Account(int account_number, const string& accountHolderName, string accountType);
+    unsigned int getAccountNumber() const;
+    string getName() const;
+    int getBalance() const;
+    string getAccountType() const;
 };
--- Bank.h
+++ Bank.h
@@ -1,26 +1,18 @@
 //Unordered Map Interface
 #pragma once
-#include<string>
+#include <unordered_map>>
 #include "Account.h"
 
 class Bank{
   private:
-  // Need to make an account number generator
-   
-    int mapSize;
-    void copyMap(int* &this_map, int* copying_map); //helper function for copy constructor and overloaded assignment operator
-    void deleteMap(int* ptr); //helper function for destructor and overloaded assignment operator
-    int* find(int account_num); //helper function to find account
+    std::unordered_map<int, Account> accounts;
     
-public:
-    Bank(); //constructor
-    Bank(const Bank &tree); //destructor
-    Bank& operator=(const Bank &tree); //overloaded assignmnet operator
-    ~Bank(); //destructor
-    void insert(int account_num, string name, int balance, string account_type); //insert account into bank
-    int remove(int account_num); //remove account from bank
-    void search(int account_num) const; //view account information 
-    void update(int account_num); //update account information
-    int size() const; //number of accounts in the bank 
-    void print(int* ptr) const; //print of all accounts in the bank
+  public:
+    Bank();
+    void createAccount(int account_num, const string& name, string& account_type); //create bank account
+    int deleteAccount(int account_num); //remove account from bank
+    void viewAccount(int account_num) const; //view account information 
+    void updateAccount(int account_num); //update account information
+    int totalNumberOfAccounts() const; //number of accounts in the bank 
+    void printAllAccountNumbers() const; //print of all account numbers in the bank
 };
